9b0f9a7390895e25d14b09d6c2cc64c6fa8897e6
yuliana hendra w <27959213+yhendraw@users.noreply.github.com>
2018-07-10 00:53:59 +0700

    Update update.php

--- update.php
+++ update.php
@@ -3,10 +3,10 @@
  if(isset($_POST['update']))
  {
  $id=$_POST['id'];
- $title=$_POST['title'];
- $duration=$_POST['duration'];
- $price=$_POST['price'];
- $q=mysqli_query($con,"UPDATE `course_details` SET `title`='$title',`duration`='$duration',`price`='$price' where `id`='$id'");
+ $NAMA=$_POST['NAMA'];
+ $NIM=$_POST['NIM'];
+ $NILAI=$_POST['NILAI'];
+ $q=mysqli_query($con,"UPDATE `course_details` SET `NAMA`='$NAMA',`NIM`='$NIM',`NILAI`='$NILAI' where `id`='$id'");
  if($q)
  echo "success";
  else
