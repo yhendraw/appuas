a1a676a4c9868ac3ec7b8cf85fa9cea483cc5399
yuliana hendra w <27959213+yhendraw@users.noreply.github.com>
2018-07-10 01:15:30 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,10 +2,10 @@
  include "db.php";
  if(isset($_POST['insert']))
  {
- $title=$_POST['title'];
- $duration=$_POST['duration'];
- $price=$_POST['price'];
- $q=mysqli_query($con,"INSERT INTO `course_details` (`title`,`duration`,`price`) VALUES ('$title','$duration','$price')");
+ $NAMA=$_POST['NAMA'];
+ $NIM=$_POST['NIM'];
+ $NILAI=$_POST['NILAI'];
+ $q=mysqli_query($con,"INSERT INTO `course_details` (`NAMA`,`NIM`,`NILAI`) VALUES ('$NAMA','$NIM','$NILAI')");
  if($q)
   echo "success";
  else
